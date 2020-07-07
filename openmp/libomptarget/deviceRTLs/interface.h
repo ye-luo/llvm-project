237743624618e2a30b4922d0557d7f339d4a5890
Johannes Doerfert <johannes@jdoerfert.de>
2020-07-06 19:13:37 -0500

    [OpenMP][NFC] Remove unused (always fixed) arguments
    Summary:
    There are various runtime calls in the device runtime with unused, or
    always fixed, arguments. This is bad for all sorts of reasons. Clean up
    two before as we match them in OpenMPOpt now.
    
    Reviewers: jhuber6, fghanim, JonChesterfield, grokos, AndreyChurbanov, ye-luo, tianshilei1992, ggeorgakoudis
    
    Subscribers: jholewinski, yaxunl, bollu, guansong, sstefan1, cfe-commits, llvm-commits, openmp-commits
    
    Tags: #clang, #openmp, #llvm
    
    Differential Revision: https://reviews.llvm.org/D83268

--- openmp/libomptarget/deviceRTLs/interface.h
+++ openmp/libomptarget/deviceRTLs/interface.h
@@ -475,10 +475,8 @@ EXTERN void __kmpc_spmd_kernel_init(int ThreadLimit, int16_t RequiresOMPRuntime,
                                     int16_t RequiresDataSharing);
 EXTERN __attribute__((deprecated)) void __kmpc_spmd_kernel_deinit();
 EXTERN void __kmpc_spmd_kernel_deinit_v2(int16_t RequiresOMPRuntime);
-EXTERN void __kmpc_kernel_prepare_parallel(void *WorkFn,
-                                           int16_t IsOMPRuntimeInitialized);
-EXTERN bool __kmpc_kernel_parallel(void **WorkFn,
-                                   int16_t IsOMPRuntimeInitialized);
+EXTERN void __kmpc_kernel_prepare_parallel(void *WorkFn);
+EXTERN bool __kmpc_kernel_parallel(void **WorkFn);
 EXTERN void __kmpc_kernel_end_parallel();
 EXTERN bool __kmpc_kernel_convergent_parallel(void *buffer,
                                               __kmpc_impl_lanemask_t Mask,
